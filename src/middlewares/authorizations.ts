15c37f0a830d93a9ac2c1cda5b1035a880763713
= <=>
2025-04-21 15:21:04 -0300

    routes

--- src/middlewares/authorizations.ts
+++ src/middlewares/authorizations.ts
@@ -1,29 +1,22 @@
-import type { User } from '../models'
+import type { Role, User } from '../models'
 import type { AuthenticatedRequest } from './authentications'
 import { ServerResponse } from 'http'
 
-/**
- * Middleware to authorize a user based on their role.
- *
- * @param {AuthenticatedRequest} req - The incoming HTTP request, with a verified user attached.
- * @param {ServerResponse} res - The HTTP server response object.
- * @param {string[]} roles - An array of allowed roles for accessing the resource.
- * @returns {Promise<boolean>} - Returns `true` if the user's role is authorized.
- * Otherwise, sends a 403 Forbidden response and returns `false`.
- */
-export const authorizeRoles = async (
-  req: AuthenticatedRequest,
-  res: ServerResponse,  
-  roles: string[]
-): Promise<boolean> => {
-  const userRole = (req.user as User).role
 
-  if (!userRole || !roles.includes(userRole)) {
-    res.statusCode = 403
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ message: 'Forbidden' }))
-    return false
-  }
+export const authorizeRoles = (...allowedRoles: Role[]) => {
+  return async (
+    req: AuthenticatedRequest,
+    res: ServerResponse
+  ): Promise<boolean> => {
+    const user = req.user as User;
 
-  return true
-}
+    if (!user || !user.role || !allowedRoles.includes(user.role)) {
+      res.statusCode = 403;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ message: "Forbidden" }));
+      return false;
+    }
+
+    return true;
+  };
+};
